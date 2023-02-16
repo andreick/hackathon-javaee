4d2db895bb6ac6d7d6b8a85e130bc1a44d541278
Andreick <andreick.gomes.ismart@gmail.com>
2023-02-16 18:52:08 -0300

    ~ Adiciona o id do usuário no retorno da API

--- src/main/java/com/stefanini/dto/usuario/UsuarioDetailsDTO.java
+++ src/main/java/com/stefanini/dto/usuario/UsuarioDetailsDTO.java
@@ -7,20 +7,26 @@ import java.time.LocalDate;
 
 public class UsuarioDetailsDTO {
 
+    private final Long id;
     private final String nome;
     private final String email;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     private final LocalDate dataNascimento;
 
-    public UsuarioDetailsDTO(String nome, String email, LocalDate dataNascimento) {
+    public UsuarioDetailsDTO(Long id, String nome, String email, LocalDate dataNascimento) {
+        this.id = id;
         this.nome = nome;
         this.email = email;
         this.dataNascimento = dataNascimento;
     }
 
     public UsuarioDetailsDTO(Usuario usuario) {
-        this(usuario.getNome(), usuario.getEmail(), usuario.getDataNascimento());
+        this(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getDataNascimento());
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getNome() {
