6f0b369a33f31efa4e73de3cc0256bcbdba552f1
Andreick <andreick.gomes.ismart@gmail.com>
2023-02-16 13:03:52 -0300

    ~ Completa o padrão DAO
    Adiciona uma interface para as operações CRUD de uma DAO.

--- src/main/java/com/stefanini/dao/GenericDAO.java
+++ src/main/java/com/stefanini/dao/GenericDAO.java
@@ -1,15 +1,16 @@
 package com.stefanini.dao;
 
-import org.hibernate.exception.ConstraintViolationException;
-
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.transaction.Transactional;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
-public class GenericDAO<T, I>  {
+public class GenericDAO<T, I> implements CrudDAO<T, I> {
 
     @PersistenceContext(unitName = "PU")
     EntityManager em;
@@ -17,7 +18,7 @@ public class GenericDAO<T, I>  {
     Class<T> clazz;
 
     public GenericDAO() {
-        clazz = ((Class) ((ParameterizedType) getClass().getSuperclass().getGenericSuperclass()).getActualTypeArguments()[0]);
+        clazz = ((Class<T>) ((ParameterizedType) getClass().getSuperclass().getGenericSuperclass()).getActualTypeArguments()[0]);
     }
 
     @Transactional
