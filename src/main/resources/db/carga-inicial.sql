65dbcef32411fa822f47075d299883bd90fe43f9
Andreick <andreick.gomes.ismart@gmail.com>
2023-02-16 14:57:30 -0300

    ~ Configura conexão com o MySQL
    Cria scripts para criação das tabelas e inserção de dados.

---
+++ src/main/resources/db/carga-inicial.sql
@@ -0,0 +1,5 @@
+INSERT INTO usuarios VALUES (default, 'Jorge', 'jorge','jorge@email.com','70343450', '1970-03-04');
+INSERT INTO usuarios VALUES (default, 'Laura', 'laura', 'laura@email.com', '6912379', '1969-12-31');
+INSERT INTO usuarios VALUES (default, 'Rafael', 'rafael', 'rafael@email.com', '96724316', '1972-04-16');
+INSERT INTO usuarios VALUES (default, 'Henrique', 'henrique', 'henrique@email.com', '4961273', '1996-07-21');
+INSERT INTO usuarios VALUES (default, 'Sara', 'sara', 'sara@email.com', '491347', '1949-04-13');
