303149cf27cf818d9663fba821c321c9c5aadec3
Andreick <andreick.gomes.ismart@gmail.com>
2023-02-16 18:03:43 -0300

    Padroniza os retornos da API
    Utiliza a classe Response para controlar o status da requisição.
    Corrige null pointer quando o usuário não é encontrado.

--- src/main/java/com/stefanini/resources/UsuarioResource.java
+++ src/main/java/com/stefanini/resources/UsuarioResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,16 +26,22 @@ public class UsuarioResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<UsuarioDetailsDTO> listAll() {
+    public Response listAll() {
         List<Usuario> usuarios = usuarioDAO.listAll();
-        return usuarios.stream().map(UsuarioDetailsDTO::new).collect(Collectors.toList());
+        var dtos = usuarios.stream().map(UsuarioDetailsDTO::new).collect(Collectors.toList());
+        return Response.ok(dtos).build();
     }
 
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public UsuarioDetailsDTO read(@PathParam("id") Long id) {
+    public Response read(@PathParam("id") Long id) {
         Usuario usuario = usuarioDAO.findById(id);
-        return new UsuarioDetailsDTO(usuario);
+        if (usuario == null) {
+            String message = "Usuário com id " + id + " não encontrado";
+            return Response.status(404).entity(message).build();
+        }
+        var dto = new UsuarioDetailsDTO(usuario);
+        return Response.ok(dto).build();
     }
 }
