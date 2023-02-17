3f1fe120fd0d5cae92066942c378fb042ae14ade
Andreick <andreick.gomes.ismart@gmail.com>
2023-02-17 17:15:43 -0300

    ~ Retira responsabilidades da UsuarioResource
    Mantém o tratamento dos DTOs e das regras de negócio na camada de serviço.

--- src/main/java/com/stefanini/resources/UsuarioResource.java
+++ src/main/java/com/stefanini/resources/UsuarioResource.java
@@ -1,9 +1,8 @@
 package com.stefanini.resources;
 
-import com.stefanini.dao.UsuarioDAO;
 import com.stefanini.dto.usuario.UsuarioCreateDTO;
 import com.stefanini.dto.usuario.UsuarioDetailsDTO;
-import com.stefanini.model.Usuario;
+import com.stefanini.service.UsuarioService;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -12,51 +11,35 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Path("/usuarios")
 public class UsuarioResource {
 
-    private final UsuarioDAO usuarioDAO;
-
     @Inject
-    public UsuarioResource(UsuarioDAO usuarioDAO) {
-        this.usuarioDAO = usuarioDAO;
-    }
+    private UsuarioService usuarioService;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response create(UsuarioCreateDTO dto, @Context UriInfo uriInfo) {
-        var usuario = new Usuario(dto);
-        usuarioDAO.save(usuario);
-
+        UsuarioDetailsDTO usuario = usuarioService.save(dto);
         var uriBuilder = uriInfo.getBaseUriBuilder();
-        uriBuilder.path(usuario.getId().toString());
-
-        var body = new UsuarioDetailsDTO(usuario);
-
-        return Response.created(uriBuilder.build()).entity(body).build();
+        uriBuilder.path(usuario.getId().toString()); // Cabeçalho Location
+        return Response.created(uriBuilder.build()).entity(usuario).build();
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listAll() {
-        List<Usuario> usuarios = usuarioDAO.listAll();
-        var dtos = usuarios.stream().map(UsuarioDetailsDTO::new).collect(Collectors.toList());
-        return Response.ok(dtos).build();
+    public Response readAll() {
+        List<UsuarioDetailsDTO> usuarios = usuarioService.listAll();
+        return Response.ok(usuarios).build();
     }
 
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response read(@PathParam("id") Long id) {
-        Usuario usuario = usuarioDAO.findById(id);
-        if (usuario == null) {
-            String message = "Usuário com id " + id + " não encontrado";
-            return Response.status(404).entity(message).build();
-        }
-        var dto = new UsuarioDetailsDTO(usuario);
-        return Response.ok(dto).build();
+    public Response readOne(@PathParam("id") Long id) {
+        UsuarioDetailsDTO usuario = usuarioService.findById(id);
+        return Response.ok(usuario).build();
     }
 }
